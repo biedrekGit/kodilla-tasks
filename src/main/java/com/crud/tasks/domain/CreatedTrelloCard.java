35fe015ad83e84397fbe474953c317ae827d45c5
biedrekGit <128526304+biedrekGit@users.noreply.github.com>
2025-06-21 14:52:49 +0200

    21.4. Add: TrelloConfig/TrelloCLientTest; Mod: TrelloController/CreatedTrelloCard/TrelloBoardDto/TrelloCardDto/TrelloClient

--- src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
+++ src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CreatedTrelloCard {
@@ -21,4 +19,10 @@ public class CreatedTrelloCard {
 
     @JsonProperty("badges")
     private TrelloBadges badges;
+
+    public CreatedTrelloCard(String id, String name, String shortUrl) {
+        this.id = id;
+        this.name = name;
+        this.shortUrl = shortUrl;
+    }
 }
